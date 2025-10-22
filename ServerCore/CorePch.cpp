c26fa0a4e548ff01865b092dcad476bfabda812f
Yoonsang Lee <ysle@pm.me>
2025-10-23 03:56:12 +0900

    Add project files.

---
+++ ServerCore/CorePch.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "CorePch.h"
+
+void HelloWorld()
+{
+	spdlog::info("hello world from ServerCore!");
+}
---
+++ ServerCore/Types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+using Byte = unsigned char;
+using i8 = __int8;
+using i16 = __int16;
+using i32 = __int32;
+using i64 = __int64;
+using u8 = unsigned __int8;
+using u16 = unsigned __int16;
+using u32 = unsigned __int32;
+using u64 = unsigned __int64;
